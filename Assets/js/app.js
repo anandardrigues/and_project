c439db8d53b7fcba3601e90804279b9d6bc807a6
Ananda Rodrigues <rodriguesananda5@gmail.com>
2024-06-29 17:17:35 -0300

    owl

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,20 +1,22 @@
 $(document).ready(function () {
-  //HERO SLIDER
   $("#hero-slider").owlCarousel({
     loop: true,
     margin: 0,
     nav: true,
-    items:1,
+    dots:false,
+    items: 1, 
+    smartSpeed:1000,
+    navText:['PREV','NEXT'],
     responsive: {
       0: {
-       
+        
       },
       600: {
-       
+        
       },
       1000: {
-       
-      },
-    },
+        
+      }
+    }
   });
 });
